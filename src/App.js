5d89496c038b8399b67d030224e4e94b255229c2
divakaivan <54508530+divakaivan@users.noreply.github.com>
2020-07-22 18:53:47 +0300

    add Map component and circles to it for the cases

--- src/App.js
+++ src/App.js
@@ -12,12 +12,19 @@ import Map from "./Map";
 import Table from "./Table";
 import { sortData } from "./util";
 import LineGraph from "./LineGraph";
+import "leaflet/dist/leaflet.css";
 
 const App = () => {
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState("worldwide");
   const [countryInfo, setCountryInfo] = useState({});
   const [tableData, setTableData] = useState([]);
+  const [mapCenter, setMapCenter] = useState({
+    lat: 34.80746,
+    lng: -40.4796,
+  });
+  const [mapZoom, setMapZoom] = useState(3);
+  const [mapCountries, setMapCountries] = useState([]);
 
   useEffect(() => {
     fetch("https://disease.sh/v3/covid-19/all")
@@ -39,6 +46,7 @@ const App = () => {
 
           const sortedData = sortData(data);
 
+          setMapCountries(data);
           setCountries(countryInfo);
           setTableData(sortedData);
         });
@@ -60,6 +68,8 @@ const App = () => {
       .then((data) => {
         setCountry(countryCode);
         setCountryInfo(data);
+        setMapCenter([data.countryInfo.lat, data.countryInfo.long]);
+        setMapZoom(4);
       });
   };
 
@@ -102,7 +112,7 @@ const App = () => {
           />
         </div>
 
-        <Map />
+        <Map countries={mapCountries} center={mapCenter} zoom={mapZoom} />
       </div>
       <Card className="app__right">
         <CardContent>
--- src/Map.js
+++ src/Map.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { Map as LeafletMap, TileLayer } from "react-leaflet";
 
-function Map() {
+import { showDataOnMap } from "./util";
+import "./Map.css";
+
+function Map({ countries, casesType, center, zoom }) {
   return (
     <div className="map">
-      <h1>I am a map</h1>
+      <LeafletMap center={center} zoom={zoom}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        {showDataOnMap(countries, casesType)}
+      </LeafletMap>
     </div>
   );
 }
