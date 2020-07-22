e237c4941c0c652054a64f16934a1c02fc64e573
divakaivan <54508530+divakaivan@users.noreply.github.com>
2020-07-22 21:00:21 +0300

    make the map not zoom out to oblivion

--- src/Map.js
+++ src/Map.js
@@ -1,13 +1,23 @@
 import React from "react";
+import Leaflet from "leaflet";
 import { Map as LeafletMap, TileLayer } from "react-leaflet";
 
 import { showDataOnMap } from "./util";
 import "./Map.css";
 
 function Map({ countries, casesType, center, zoom }) {
+  const corner1 = Leaflet.latLng(-120, -240);
+  const corner2 = Leaflet.latLng(120, 240);
+  const bounds = Leaflet.latLngBounds(corner1, corner2);
   return (
     <div className="map">
-      <LeafletMap center={center} zoom={zoom}>
+      <LeafletMap
+        minZoom={1.5}
+        maxBounds={bounds}
+        maxBoundsViscosity={1.0}
+        center={center}
+        zoom={zoom}
+      >
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
